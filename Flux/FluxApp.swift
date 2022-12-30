74345ea518c2e3894d26e14517822c2c37c384e6
Pranav Grandhi <pranavgrandhi@pranavs-mbp.lan>
2022-12-29 23:36:50 -0800

    Xcode project

---
+++ Flux/FluxApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluxApp.swift
+//  Flux
+//
+//  Created by Pranav Grandhi on 12/29/22.
+//
+
+import SwiftUI
+
+@main
+struct FluxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
